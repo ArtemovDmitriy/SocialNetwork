e77be3b819a11269c48ed4a13223ffd78e171b83
Дмитрий <artemovdmitriy@mail.ru>
2025-02-06 12:51:15 +0300

    Добавьте файлы проекта.

---
+++ SocialNetwork/BLL/Services/UserService.cs
@@ -0,0 +1,60 @@
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.DAL.Entities;
+using SocialNetwork.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.BLL.Services
+{
+    public class UserService
+    {
+        IUserRepository userRepository;
+
+        public UserService()
+        {
+            userRepository = new UserRepository();
+        } 
+        
+        public void Register(UserRegistrationData userRegistrationData)
+        {
+            if (String.IsNullOrEmpty(userRegistrationData.FirstName))
+                throw new ArgumentNullException();
+
+            if (String.IsNullOrEmpty(userRegistrationData.LastName))
+                throw new ArgumentNullException();
+
+            if (String.IsNullOrEmpty(userRegistrationData.Password))
+                throw new ArgumentNullException();
+
+            if (String.IsNullOrEmpty(userRegistrationData.Email))
+                throw new ArgumentNullException();
+
+            if (userRegistrationData.Password.Length > 8)
+                throw new ArgumentException();
+
+            if (!new EmailAddressAttribute().IsValid(userRegistrationData.Email))
+                throw new ArgumentException();
+
+            if (userRepository.FindByEmail(userRegistrationData.Email) != null)
+                throw new ArgumentException();
+
+            var userEntity = new UserEntity()
+            {
+                firstname = userRegistrationData.FirstName,
+                lastname = userRegistrationData.LastName,
+                password = userRegistrationData.Password,
+                email = userRegistrationData.Email,
+            };
+
+            if (this.userRepository.Create(userEntity) == 0)
+                throw new Exception();
+            
+
+        }
+    }
+}
